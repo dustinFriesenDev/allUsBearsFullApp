484fda7d410cde842d195c0d03e6d17dc84332a7
Dustin Dev <94726570+dustinFriesenDev@users.noreply.github.com>
2023-11-14 10:59:37 -0500

    first commit

---
+++ src/main/java/com/MVC/allUsBears/AllUsBearsApplication.java
@@ -0,0 +1,13 @@
+package com.MVC.allUsBears;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllUsBearsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllUsBearsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MVC/allUsBears/AllUsBearsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MVC.allUsBears;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllUsBearsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
